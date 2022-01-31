601aff73c3dab14354f06a17637d65c56057fc90
samar19 <47981746+samar19@users.noreply.github.com>
2022-01-31 15:23:26 +0200

    Update sample-script.js

--- scripts/sample-script.js
+++ scripts/sample-script.js
@@ -1,31 +1,33 @@
 // We require the Hardhat Runtime Environment explicitly here. This is optional
 // but useful for running the script in a standalone fashion through `node <script>`.
+//
 // When running the script with `npx hardhat run <script>` you'll find the Hardhat
 // Runtime Environment's members available in the global scope.
-const hre = require("hardhat");
+import { ethers, upgrades } from "hardhat";
 
 async function main() {
   // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
+  // line interface. 
   //
   // If this script is run directly using `node` you may want to call compile
   // manually to make sure everything is compiled
   // await hre.run('compile');
 
   // We get the contract to deploy
-  const Greeter = await hre.ethers.getContractFactory("Greeter");
-  const greeter = await Greeter.deploy("Hello, Hardhat!");
+  const ETHPool = await ethers.getContractFactory("ETHPool");
+  console.log("Deploying ETHPool...");
+  const ETHPool = await upgrades.deployProxy(ETHPool,[] , {
+    initializer: "initialize",
+  });
 
-  await greeter.deployed();
+  await ETHPool.deployed();
 
-  console.log("Greeter deployed to:", greeter.address);
+  console.log("ETHPool deployed to:", ETHPool.address);
 }
 
 // We recommend this pattern to be able to use async/await everywhere
 // and properly handle errors.
-main()
-  .then(() => process.exit(0))
-  .catch((error) => {
-    console.error(error);
-    process.exit(1);
-  });
+main().catch((error) => {
+  console.error(error);
+  process.exitCode = 1;
+});
